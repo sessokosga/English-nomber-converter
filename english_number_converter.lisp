dead69a29b795b8edcd25f335350685e52714a0a
Sesso Kosga <kosgasesso@gmail.com>
2023-06-18 17:13:44 +0100

    Improved conversion up to 999

--- english_number_converter.lisp
+++ english_number_converter.lisp
@@ -30,6 +30,15 @@
 	       (return-from 20-to-99 "Enter a number between 20 and 99")))
 		   
 (defun 0-to-99 (number)
+		(if (or (< number 0) (> number 99)) (return-from 0-to-99 "Enter a number between 0 and 99") )
 	   (if (and (>= number 0) (<= number 9)) (return-from 0-to-99 (0-to-9 number)))
 	   (if (and (>= number 10) (<= number 19)) (return-from 0-to-99 (10-to-19 number)))
 	   (if (and (>= number 20) (<= number 99)) (return-from 0-to-99 (20-to-99 number))))		   
+	   
+(defun 100-to-999 (number)
+	   (if (or (< number 100) (> number 999)) (return-from 100-to-999 "Enter a number between 100 and 999"))
+	   (let ((d (floor (/ number 100)))
+		 (c (mod number 100)))
+	     (if (= c 0)
+		 (return-from 100-to-999 (concatenate 'string (0-to-99 d) " hundred"))
+		 (return-from 100-to-999 (concatenate 'string (0-to-99 d) " hundred " (0-to-99 c))))))
