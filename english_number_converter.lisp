8c8bac680fd6bc442ef66faefb0699b5a119952a
Sesso Kosga <kosgasesso@gmail.com>
2023-06-18 16:08:32 +0100

    Improved conversion up to 19

--- english_number_converter.lisp
+++ english_number_converter.lisp
@@ -1,8 +1,17 @@
 (defconstant units #("zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"))
+(defconstant teens #("ten" "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen"
+ "eighteen" "nineteen"))
+
+(defmacro assert-equal (num fun)
+	   `(string-equal (format nil "~r" ,num) (,fun ,num)))
+	   
 (defun 0-to-9 (number)
 	(if (and (>= number 0) (<= number 9)) 
 		(return-from 0-to-9 (aref units number))
 		(return-from 0-to-9 "Enter a number between 0 and 9")))
 
-(defmacro assert-equal (num fun)
-	   `(string-equal (format nil "~r" ,num) (,fun ,num)))
+
+(defun 10-to-19 (number)
+	   (if (and (>= number 10) (<= number 19))
+		    (return-from 10-to-19 (aref teens (- number 10)))
+		    (return-from 10-to-19 "Enter a number between 10 and 19")))	   
